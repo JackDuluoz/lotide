2346d8d0ca179fb2d40173788b93721cd4a3b942
Michael <greenmr95@hotmail.com>
2022-11-15 00:41:11 +0000

    Initial Commit

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢️🟢️🟢️ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑️🛑️🛑️ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 4);
