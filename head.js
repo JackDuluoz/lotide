386bc86849bbd21eca21217c8e014e934e74ec0e
Michael <greenmr95@hotmail.com>
2022-11-15 02:13:44 +0000

    Improved assertEqual

--- head.js
+++ head.js
@@ -1,4 +1,3 @@
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🟢️🟢️🟢️ Assertion Passed: ${actual} === ${expected}`);
@@ -11,3 +10,16 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 4);
+
+const head = function(array, zero) {
+  array.splice(1, array.length - 1);
+  for (let a of array) {
+    return array[0];
+  }
+};
+
+console.log("------------------------------------------------------------------");
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]), "Hello");
