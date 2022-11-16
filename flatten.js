66d8a94469a6e85a6f024c414681aec9be3cb12a
Michael <greenmr95@hotmail.com>
2022-11-16 08:24:15 +0000

    Function flattens nested array elements into a new, single array of only numbers

---
+++ flatten.js
@@ -0,0 +1,20 @@
+const flatten = function(nestedArrays) {
+  let output = [];
+  let flattenedArray = Array.from(nestedArrays.toString().replace(/,/g, ""));
+  for (let f = 0; f < flattenedArray.length; f++) {
+    if (parseInt(flattenedArray[f], 10)) {
+      output.push(flattenedArray[f]);
+    }
+  }
+  for (let o = 0; o < output.length; o++) {
+    output[o] = parseInt(output[o], 10);
+  }
+  console.log(output);
+};
+flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
+flatten([1, 2, [3, 4], "five", [6]]); // => [1, 2, 3, 4, 5, 6]
+flatten([1, 2, [3, 4], null, [6]]); // => [1, 2, 3, 4, 5, 6]
+flatten([1, 2, [3, 4], [null], [6]]); // => [1, 2, 3, 4, 5, 6]
+flatten([1, 2, [3, 4], true, [6]]); // => [1, 2, 3, 4, 5, 6]
+flatten([1, 2, [3, 4], undefined, [6]]); // => [1, 2, 3, 4, 5, 6]
+flatten([1, 2, [3, 4], "5", [6]]); // => [1, 2, 3, 4, 5, 6]
