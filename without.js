b4171854f3a581825b918f36f7ed288e1bf1bfc9
Michael <greenmr95@hotmail.com>
2022-11-16 06:04:16 +0000

    Completed without function

---
+++ without.js
@@ -0,0 +1,29 @@
+const without = function(source, itemsToRemove) {
+  let sameInput = [];
+  let output = [];
+  for (let i = 0; i < itemsToRemove.length; i++) {
+    for (let s = 0; s < source.length; s++) {
+      if ((typeof itemsToRemove[i] === typeof source[s]) && (itemsToRemove[i] === source[s])) {
+        sameInput.push(itemsToRemove[i]);
+      }
+    }
+  }
+  for (let si = 0; si < sameInput.length; si++) {
+    for (let s = 0; s < source.length; s++) {
+      if (source[s] !== sameInput[si]) {
+        output.push(source[s]);
+      }
+    }
+  }
+  console.log("Source:", source);
+  console.log("Items to Remove:", itemsToRemove);
+  console.log("Shared Inputs:", sameInput);
+  console.log("Without Shared Inputs:", output);
+  console.log("Confirm Source Unchanged:", source);
+};
+without([1, 2, 3], [1]);
+console.log("-----------------------------------------");
+without(["1", "2", "3"], [1, 2, "3"]);
+console.log("-----------------------------------------");
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
