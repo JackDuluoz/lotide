44b5b6581746e74076c121e255498b652e98f574
Michael <greenmr95@hotmail.com>
2022-11-17 05:52:32 +0000

    function lists the indices of letters occuring in a given string

---
+++ letterPositions.js
@@ -0,0 +1,17 @@
+const letterPositions = function(sentence) {
+  let output = {};
+  let duplicate = sentence;
+  for (let letter of sentence) {
+    let indices = [];
+    if (letter !== " ") {
+      for (let d = 0; d < duplicate.length; d++) {
+        if (letter === duplicate[d]) {
+          indices.push(d);
+        }
+      }
+      output[letter] = [indices];
+    }
+  }
+  return output;
+};
+console.log(letterPositions("Hello Michael"));
