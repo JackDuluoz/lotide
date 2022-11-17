fa7c07e98f05834732c5dcbc5b3b9f9658b6067c
Michael <greenmr95@hotmail.com>
2022-11-17 05:35:07 +0000

    function that keeps a running count of letters in a string

---
+++ countLetters.js
@@ -0,0 +1,18 @@
+const countLetters = function(sentence) {
+  let output = {};
+  let duplicate = sentence;
+  for (let letter of sentence) {
+    let count = 0;
+    if (letter !== " ") {
+      for (let d of duplicate) {
+        if (letter === d) {
+          count = count + 1;
+        }
+      }
+      output[letter] = count;
+    }
+  }
+  return output;
+};
+
+console.log(countLetters("lighthouse labs in the house"));
