4ead1e1694c6fe46ffdad78e8ebdf8441acd369d
Michael <greenmr95@hotmail.com>
2022-11-17 04:12:40 +0000

    reports a running count of specified items occurring in a list

---
+++ countOnly.js
@@ -0,0 +1,33 @@
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Fang",
+  "Fang",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const countOnly = function(allItems, itemsToCount) {
+  let result = {};
+  for (let item in itemsToCount) {
+    if (itemsToCount[item] === true) {
+      let count = 0;
+      for (let name of allItems) {
+        if (name === item) {
+          count = count + 1;
+        }
+      }
+      if (count > 0) {
+        result[item] = count;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(countOnly(firstNames, { "Jason": true, "Karima": true, "Salima": false, "Fang": true, "Agouhanna": true }));
