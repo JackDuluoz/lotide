9f8a3f9b035d9093d7bd7d41afb133050a8f3e3c
Michael <greenmr95@hotmail.com>
2022-11-18 09:16:43 +0000

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,40 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("True");
+const eqArrays = function (firstArray, secondArray) {
+  const inspect = require('util').inspect;
+  if (inspect(firstArray) === inspect(secondArray)) {
+    console.log(`🟢️ Assertion Passed: ${inspect(firstArray)} === ${inspect(secondArray)}`)
   } else {
-    console.log("False");
+    console.log(`🛑️ Assertion Failed: ${inspect(firstArray)} !== ${inspect(secondArray)}`)
   }
-};
-const eqArrays = function(firstArray, secondArray) {
-  let firstTypes = [];
-  let secondTypes = [];
-  let firstString;
-  let secondString;
-  let truth;
-  for (let fa = 0; fa < firstArray.length; fa++) {
-    firstTypes.push(typeof firstArray[fa]);
-  }
-  for (let sa = 0; sa < secondArray.length; sa++) {
-    secondTypes.push(typeof secondArray[sa]);
-  }
-  let firstTypesString = firstTypes.join(", ");
-  let secondTypesString = secondTypes.join(", ");
-  if (firstTypesString === secondTypesString) {
-    firstString = firstArray.join("");
-    secondString = secondArray.join("");
-  } else {
-    firstString = "one thing";
-    secondString = "another thing";
-  }
-  if (firstString === secondString) {
-    truth = true;
-  } else {
-    truth = false;
-  }
-  return truth;
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+}
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+console.log("")
