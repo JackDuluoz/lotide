f87491a04aae5ba7e16de300eb04d305d154af30
Michael <greenmr95@hotmail.com>
2022-11-22 00:44:19 +0000

    tailTest converted to mocha/chai test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,28 @@
-const tail = require('../tail')
-const assertEqual = require('../assertEqual')
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-// This will always fail as the contents of arrays cannot be compared in this way.
-console.log("");
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-console.log("");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log("");
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+// const assertEqual = require('../assertEqual')
+
+describe("#tail", () => {
+  it("returns 2 for .length of [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2)
+  })
+  it("returns Lighthouse for [0] of [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse")
+  })
+  it("returns Labs for [1] of [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs")
+  })
+  it("returns Lighthouse Labs for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+  })
+})
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// //assertEqual(result, ["Lighthouse", "Labs"]);
+// // This will always fail as the contents of arrays cannot be compared in this way.
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
